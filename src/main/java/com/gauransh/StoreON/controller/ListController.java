f58094ad2ce3a481d9c82fccfa31feeecef8ae27
GauranshSingh <gauranshsinghse@gmail.com>
2025-07-07 09:18:59 +0530

    Initiating project in github

--- src/main/java/com/gauransh/StoreON/controller/ListController.java
+++ src/main/java/com/gauransh/StoreON/controller/ListController.java
@@ -1,26 +1,30 @@
 //
 package com.gauransh.StoreON.controller;
-//
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.ui.Model;
-//import org.springframework.web.bind.annotation.GetMapping;
-//
-//import com.gauransh.StoreON.entity.Product;
-//import com.gauransh.StoreON.repository.ProductRepository;
-//
-//import jakarta.servlet.http.HttpSession;
-//@Controller
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import com.gauransh.StoreON.entity.Product;
+import com.gauransh.StoreON.repository.ProductRepository;
+
+import jakarta.servlet.http.HttpSession;
+@Controller
 public class ListController {
 	
-//	@Autowired
-//	private ProductRepository Product;
-//	
-//	@GetMapping("/List")
-//	public String getdata(HttpSession session, Model model){
-//		List<Product> product = Product.findAll();
-//		String loggedInUser =(String) session.getAttribute("loggedInUser");
-//		model.addAttribute("loggedInUser",loggedInUser);
-//		return "List";
-//	}
+	@Autowired
+	private ProductRepository Product;
+	
+	@GetMapping("/List")
+	public String getdata(HttpSession session, Model model){
+		List<Product> products = Product.findAll();
+	    model.addAttribute("products", products);
+		String loggedInUser =(String) session.getAttribute("loggedInUser");
+		System.out.println(products);
+
+		model.addAttribute("loggedInUser",loggedInUser);
+		return "List";
+	}
 }
--- src/main/java/com/gauransh/StoreON/repository/ProductRepository.java
+++ src/main/java/com/gauransh/StoreON/repository/ProductRepository.java
@@ -1,10 +1,10 @@
 package com.gauransh.StoreON.repository;
 
-//import org.springframework.data.jpa.repository.JpaRepository;
-//import com.gauransh.StoreON.entity.Product;
 
-//public interface ProductRepository extends JpaRepository<Product, String> {
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.gauransh.StoreON.entity.Product;
 
-public interface ProductRepository{
+public interface ProductRepository extends JpaRepository<Product, String> {
+//		List<Product> findAll();
 
 }
